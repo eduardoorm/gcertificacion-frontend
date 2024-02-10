300d6b5867640dceff9a15016e0a2ba46a1ee346
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2024-02-10 01:43:59 -0500

    fix: click redirecciona a paginas documentacion y inducción correctamente

--- src/views/trabajador/default/index.tsx
+++ src/views/trabajador/default/index.tsx
@@ -1,42 +1,40 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import { useNavigate, useParams } from "react-router-dom";
 import { Clase, TIPO_CLASE } from "../../../interfaces/entities";
-import { RootState, getClasesByTrabajador, useAppDispatch, useAppSelector } from "../../../store";
-import { useAPIData } from "../../../api/useAPIData";
+import { getClasesByTrabajador, useAppDispatch, useAppSelector } from "../../../store";
 import { useAuthUser } from 'react-auth-kit'
 import HeaderTrabajadorView from "../header";
-import moment from "moment";
 import PaperClasses from "../../../components/PaperClasses/PaperClasses";
-import { useSelector } from 'react-redux';
 
 export default function ViewTrabajadorDefault(){
-    //const { clases: clasesReducer } = useAppSelector((state:RootState) => state.clases);
-    //const [inducciones, setInducciones] = React.useState<Clase[]>([]);
-    //const [capacitaciones, setCapacitaciones] = React.useState<Clase[]>([]);
-    //const [documentaciones, setDocumentaciones] = React.useState<Clase[]>([]);
+
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
     const auth = useAuthUser();
     const clases = useAppSelector((state) => state.clases.clases.data);
-    const { id, clienteId } = useParams();
 
     React.useEffect(() => {
         dispatch(getClasesByTrabajador(auth()?.id_trabajador || '0'));
     }, []);
 
     const handleClick = (event: React.MouseEvent<unknown>, clase: Clase) => {
-        navigate(
-            `/trabajador/${clase.tipo}/${clase.id}`,
-            { replace: false }
-        );
+        if(clase.tipo==TIPO_CLASE.DOCUMENTACION){
+            navigate(
+                `/trabajador/documentacion`,
+                { replace: false }
+            );
+        }else if(clase.tipo==TIPO_CLASE.INDUCCION){
+            navigate(
+                `/trabajador/induccion`,
+                { replace: false }
+            );
+        }else if(clase.tipo==TIPO_CLASE.CAPACITACION){
+            navigate(
+                `/trabajador/${clase.tipo}/${clase.id}`,
+                { replace: false }
+            );
+        }
     };
 
     return (
