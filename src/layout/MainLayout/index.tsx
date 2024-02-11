42fc8645f1af2410e971b63a4f463b5637f3e4f5
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2024-02-11 04:54:11 -0500

    feat: componente menu-permanente agregado

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -1,23 +1,19 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { Logout } from '@mui/icons-material';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Avatar,Container, ListItemIcon, Menu, MenuItem } from '@mui/material';
 import { useSignOut } from 'react-auth-kit'
-import { AdminListItems, TrabajadorListItems, EmpresaListItems } from './listItems';
 import { TIPO_USUARIO, UserAuthenticated } from '../../interfaces/entities';
 import { RootState, setUserAuthenticated, useAppDispatch, useAppSelector } from '../../store';
 import brand from '../../assets/images/brand.jpg';
+import MenuPermanente from '../../components/Util/MenuPermanente/MenuPermanente';
 
 const initialStateUserAuthenticated: UserAuthenticated = {
     id_trabajador: 0,
@@ -54,31 +50,6 @@ const AppBar = styled(MuiAppBar, {
     }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        '& .MuiDrawer-paper': {
-            position: 'relative',
-            whiteSpace: 'nowrap',
-            width: drawerWidth,
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            boxSizing: 'border-box',
-            ...(!open && {
-                overflowX: 'hidden',
-                transition: theme.transitions.create('width', {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-                width: theme.spacing(7),
-                [theme.breakpoints.up('sm')]: {
-                    width: theme.spacing(9),
-                },
-            }),
-        },
-    }),
-);
 
 
 export default function MainLayout() {
@@ -187,34 +158,7 @@ export default function MainLayout() {
                     </Menu>
                 </Toolbar>
             </AppBar>
-            <Drawer variant="permanent" open={openAppBar}>
-                <Toolbar
-                    sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-end',
-                      px: [1],
-                    }}
-                >
-                    <img src={brand} alt="Global Certificación" width={'75%'} />
-                    <IconButton onClick={toggleDrawer}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </Toolbar>
-                <Divider />
-                <List component="nav">
-                    {userAuthenticated.tipo === TIPO_USUARIO.ADMIN && 
-                    <AdminListItems/>}
-                    {userAuthenticated.tipo === TIPO_USUARIO.ADMIN && 
-                    <Divider sx={{ my: 1 }} /> }
-                    {userAuthenticated.tipo === TIPO_USUARIO.TRABAJADOR && 
-                    <TrabajadorListItems title='' /> }
-                    {userAuthenticated.tipo === TIPO_USUARIO.TRABAJADOR && 
-                    <Divider sx={{ my: 1 }} />}
-                    {userAuthenticated.tipo === TIPO_USUARIO.EMPRESA && 
-                    <EmpresaListItems/>}
-                </List>
-            </Drawer>
+            <MenuPermanente openAppBar={openAppBar} brand={brand} toggleDrawer={toggleDrawer} userAuthenticated={userAuthenticated} />
             <Box
                 component="main"
                 sx={{
